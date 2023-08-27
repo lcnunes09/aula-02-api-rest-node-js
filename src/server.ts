f07b9465c30a6b7121e6df0026da409bfbf0fbf7
Luciana Nunes <lcnunes09@gmail.com>
2023-08-27 13:00:28 -0500

    feat: create transactions route plugin

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,10 @@
 import fastify from 'fastify'
-import { knex } from './database'
 import { env } from './env' 
-
-import crypto from 'node:crypto'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-    const transaction = await knex('transactions').insert({
-        id: crypto.randomUUID(),
-        title: 'Título',
-        amount: 1000,
-    }).returning('*')
-
-    return transaction
-})
+app.register(transactionsRoutes)
 
 app.listen({
     port: env.PORT,
